4a0312d825a7141dc677bd9b4424964724588c32
Bálint-István Kolozsvári <kolozsvari0611@gmail.com>
2025-12-16 18:54:53 +0200

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+
+namespace Kolozsvari_Balint_Istvan_Lab2.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+
+namespace Kolozsvari_Balint_Istvan_Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
